ef8958dccf016027488cd92500a3fcdd16e8613c
gnarfugh <kmorales076@gmail.com>
2016-10-27 14:57:40 -0500

    fix linkedin link

---
+++ _site/js/script.js
@@ -0,0 +1,18 @@
+$(function() {
+  $("button").click(function() {
+    $(".hamburger").toggleClass("focus");
+    $(".content").toggleClass("show");
+  });
+
+  $("nav").click(function() {
+    $(".content").toggleClass('show');
+    $(".hamburger").toggleClass("focus");
+  });
+});
+
+// i like pizza
+(function addPizza(){
+	if (navigator.userAgent.indexOf('Mac OS X') != -1) {
+    window.location.hash = "🍕";
+  }
+})();
