3b9e538a9a3b4fc67940668bb2fa465b2d4adf35
gnarfugh <kmorales076@gmail.com>
2016-11-01 16:43:25 -0500

    lazy load images

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-(function ($, window, document, undefined) {
+$(function() {
   $("button").click(function() {
     $(".hamburger").toggleClass("focus");
     $(".content").toggleClass("show");
@@ -8,25 +8,11 @@
     $(".content").toggleClass('show');
     $(".hamburger").toggleClass("focus");
   });
+});
 
 // i like pizza
-(function addPizza() {
+(function addPizza(){
   if (navigator.userAgent.indexOf('Mac OS X') != -1) {
     window.location.hash = "🍕";
   }
 })();
-
-// defer CSS loading
-var cb = function() {
-  var l = document.createElement('link');
-  l.rel = 'stylesheet';
-  l.href = 'css/main.min.css';
-  var h = document.getElementsByTagName('head')[0];
-  h.parentNode.insertBefore(l, h);
-};
-var raf = requestAnimationFrame || mozRequestAnimationFrame ||
-  webkitRequestAnimationFrame || msRequestAnimationFrame;
-if (raf) raf(cb);
-else window.addEventListener('load', cb);
-
-})(jQuery, window, document);
