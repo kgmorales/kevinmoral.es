aa85cbfcd8e6566ca1b2148e2bbcec961ab6f33a
kgmorales <kmorales076@gmail.com>
2020-08-14 10:39:38 -0500

    Update 2020-08-14T15:39:37.498Z

--- scripts/script.js
+++ scripts/script.js
@@ -12,9 +12,9 @@ $(function () {
 
 (function addMe() {
 	var macOSX = navigator.userAgent.indexOf('Mac OS X') !== -1;
-	var chrome = navigator.userAgent.indexOf('Chrome') !== -1;
+	var firefox = navigator.userAgent.indexOf('FireFox') !== -1;
 	var mobile = typeof window.orientation !== 'undefined';
-	if (macOSX && !chrome && !mobile) {
+	if (macOSX && firefox && !mobile) {
 		window.location.hash = '👨🏽‍💻';
 	}
 })();
