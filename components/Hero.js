1a25531ef77f75060fe48973b0112c7a5580534d
kevinmoral.es <hello@kevinmoral.es>
2025-01-28 14:44:55 -0600

    change bio

--- components/Hero.js
+++ components/Hero.js
@@ -53,7 +53,12 @@ export default function Hero({ spotify }) {
           Kevin Morales
         </h1>
         <p className="text-base text-gray-600 dark:text-gray-400">
-          I am a Full Stack Engineer with a foundation in Design and User Experience, ...
+          I am a Full Stack Engineer with a foundation in Design and User Experience, I specialize
+          in refining and optimizing code for maintainability, scalability, and reusability. My
+          attention to detail and pattern recognition enhances the efficiency of the solutions I
+          design and develop. I've taken projects from conception to deployment to maintenance,
+          collaborating with teams of varying sizes. Ensuring that the requirements of all Users and
+          stakeholders are addressed.
         </p>
         {/* 2) Pass the same object to SpotifyNowPlaying */}
         <SpotifyNowPlaying spotify={spotify} />
