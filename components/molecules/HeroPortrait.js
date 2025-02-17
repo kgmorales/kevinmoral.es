cee339e6f375dc74b2b13440713082d78a5011c3
kevinmoral.es <hello@kevinmoral.es>
2025-02-17 13:51:38 -0600

    new me

--- components/molecules/HeroPortrait.js
+++ components/molecules/HeroPortrait.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
-import portraitImage from '../../public/static/images/avatar.webp'
+import portraitImage from '../../public/static/images/me.webp'
 
 export default function HeroPortrait() {
   return (
     <>
       <Image
         priority
-        width={600}
-        height={424}
+        width={900}
+        height={500}
         placeholder="blur"
         src={portraitImage}
         alt="Portrait of Kevin Morales"
