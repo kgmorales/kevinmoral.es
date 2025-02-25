b07931b335afece5435cd0f1befebceda2087712
kevinmoral.es <hello@kevinmoral.es>
2025-02-25 10:09:27 -0600

    ga tag

--- components/analytics/GoogleAnalytics.js
+++ components/analytics/GoogleAnalytics.js
@@ -3,6 +3,7 @@ import Script from 'next/script'
 const GAScript = () => {
   return (
     <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-JK3YG6KYBM"></Script>
       <Script strategy="lazyOnload" id="ga-script">
         {`
             window.dataLayer = window.dataLayer || [];
