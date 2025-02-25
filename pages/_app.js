bdf9740b2f8bebc88dcf60ee4f26ef3caedb79c5
kevinmoral.es <hello@kevinmoral.es>
2025-02-25 09:29:45 -0600

    next head

--- pages/_app.js
+++ pages/_app.js
@@ -8,17 +8,14 @@ import Head from 'next/head'
 import siteMetadata from '@/data/siteMetadata'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
-import GAScript from '@/components/analytics/GoogleAnalytics'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
-const isProduction = process.env.NODE_ENV === 'production'
 const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
-        {isProduction && <GAScript />}
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
